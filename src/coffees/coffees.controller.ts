d1139be1f9393fb6ebb4d79bdf3baaeea1090894
cerebral-tannn11 <tan.nguyen2@cerebral.com>
2022-10-31 10:43:50 +0700

    Add PostgreSQL with TypeORM

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { PaginationQueryDto } from './dto/pagination-query.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
@@ -17,9 +18,8 @@ export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   @Get()
-  findAll(@Query() paginationQuery) {
-    // const { limit, offset } = paginationQuery;
-    return this.coffeesService.findAll();
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.coffeesService.findAll(paginationQuery);
   }
 
   @Get(':id')
